ac83618f2287e3ee67e95ac2d034147956536578
ruchitGandhiNEU <55202985+ruchitGandhiNEU@users.noreply.github.com>
2020-04-13 00:49:45 -0400

    initializing data variable in dataloader, setting setters and getters

--- src/main/java/com/ranking/loader/DataLoader.java
+++ src/main/java/com/ranking/loader/DataLoader.java
@@ -6,11 +6,14 @@
  */
 package com.ranking.loader;
 
+import com.ranking.model.Match;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  *
@@ -19,21 +22,37 @@ import java.util.List;
 public class DataLoader {
     
     static String fileName ;
+    private static Map<String, Map<String, Map< Integer, Match>>> data;
+
+        public DataLoader(String filename) {
+        this.fileName = filename;
+        this.data = new HashMap();
+    }
     
+    
+    public static Map<String, Map<String, Map<Integer, Match>>> getData() {
+        return data;
+    }
+
+    public static void setData(Map<String, Map<String, Map<Integer, Match>>> data) {
+        DataLoader.data = data;
+    }
     
     public int[][] getMat(){
     
         int[][] res = new int[41][41];
         
+//        Map<String, Map<String, 
         List<List<String>> records = new ArrayList<>();
 try (BufferedReader br = new BufferedReader(new FileReader("book.csv"))) {
     String line;
     
     
-        line = br.readLine();
+        String[] heads = br.readLine().split(",");
         
     while ((line = br.readLine()) != null) {
         String[] values = line.split(",");
+        
         records.add(Arrays.asList(values));
     }
 }catch(Exception e){}
