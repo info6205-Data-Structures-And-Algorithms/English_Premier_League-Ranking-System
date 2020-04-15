e5cbdbb9a0cb1cd47c978cd4b61116f7e898a23e
ruchitGandhiNEU <55202985+ruchitGandhiNEU@users.noreply.github.com>
2020-04-15 04:47:58 -0400

    Updating Colley Matrix, created basic structure ready for use in Run.java

--- src/main/java/com/ranking/utility/ColleyUtil.java
+++ src/main/java/com/ranking/utility/ColleyUtil.java
@@ -5,12 +5,13 @@
  */
 package com.ranking.utility;
 
+import com.ranking.exception.DevException;
+import com.ranking.loader.DataLoader;
 import com.ranking.model.Match;
 import com.sun.javafx.geom.Matrix3f;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
-import org.apache.commons.math3.linear.Array2DRowRealMatrix;
 import org.apache.commons.math3.linear.ArrayRealVector;
 import org.apache.commons.math3.linear.DecompositionSolver;
 import org.apache.commons.math3.linear.LUDecomposition;
@@ -24,30 +25,29 @@ import org.apache.commons.math3.linear.RealVector;
  */
 public class ColleyUtil {
 
-    private Map<String, Map<String, Map< Integer, Match>>> data;
-    private Map<String, Double> ranking;
+    private Map<String, Map<String, Map< Integer, Match>>> data = DataLoader.getData();
+    private static Map<Integer, Map<String, Double>> ranking;
 
-    public ColleyUtil(Map<String, Map<String, Map< Integer, Match>>> data) {
-        this.data = data;
-        this.ranking = new HashMap();
-        System.out.println(Arrays.toString(solveForMat().toArray()));
-    }
+    public static Double getRank(int season, String teamName) {
 
-    public Map<String, Double> getRanking() {
-        return ranking;
-    }
+        Map<String, Double> teamMap = ColleyUtil.ranking.get(season);
+        if (teamMap == null || teamMap.isEmpty()) {
+            return Double.NaN;
+        }
 
-    public void setRanking(Map<String, Double> ranking) {
-        this.ranking = ranking;
+        Double rank = teamMap.get(teamName);
+
+        if (rank == null || rank.isNaN()) {
+            return Double.NaN;
+        }
+
+        return rank;
+
     }
 
     public RealVector solveForMat() {
-        
 
         RealMatrix coefficients = MatrixUtils.createRealMatrix(new double[][]{{2, 3, -2}, {-1, 7, 6}, {4, -3, -5}});
-        
-//        MatrixUtils.createRealMatrix(new double[][]{{2, 3, -2}, {-1, 7, 6}, {4, -3, -5}});
-
         DecompositionSolver solver = new LUDecomposition(coefficients).getSolver();
         RealVector constants = new ArrayRealVector(new double[]{1, -2, 1}, false);
         RealVector solution = solver.solve(constants);
@@ -56,29 +56,38 @@ public class ColleyUtil {
     }
 
     private void loadRankings() {
-        
-        int numberOfTeams = data.size();
-        RealMatrix colleyMatrix = MatrixUtils.createRealMatrix(new double[numberOfTeams][numberOfTeams]);
-        RealMatrix bMatrix = MatrixUtils.createColumnRealMatrix(new double[numberOfTeams]);
-        
-        for (Map.Entry<String, Map<String, Map<Integer, Match>>> homeMap : data.entrySet()) {
-            String homeTeam = homeMap.getKey();
-            Map<String, Map<Integer, Match>> awayMap = homeMap.getValue();
-            
-            for (Map.Entry<String, Map<Integer, Match>> a : awayMap.entrySet()) {
-                String awayTeam = a.getKey();
-                Map<Integer, Match> seasonMap = a.getValue();
-                
-                for (Map.Entry<Integer, Match> seasonData : seasonMap.entrySet()) {
-                    Integer season = seasonData.getKey();
-                    Match match = seasonData.getValue();
-                    
-                    
-                    
-                }
-                
+
+        try {
+
+            if (data == null || data.isEmpty()) {
+                throw new DevException("Map DataLoader.data is null or Empty");
             }
             
+            this.ranking = new HashMap();
+
+            int numberOfTeams = data.size();
+            RealMatrix colleyMatrix = MatrixUtils.createRealMatrix(new double[numberOfTeams][numberOfTeams]);
+            RealMatrix bMatrix = MatrixUtils.createColumnRealMatrix(new double[numberOfTeams]);
+
+            for (Map.Entry<String, Map<String, Map<Integer, Match>>> homeMap : data.entrySet()) {
+                String homeTeam = homeMap.getKey();
+                Map<String, Map<Integer, Match>> awayMap = homeMap.getValue();
+
+                for (Map.Entry<String, Map<Integer, Match>> a : awayMap.entrySet()) {
+                    String awayTeam = a.getKey();
+                    Map<Integer, Match> seasonMap = a.getValue();
+
+                    for (Map.Entry<Integer, Match> seasonData : seasonMap.entrySet()) {
+                        Integer season = seasonData.getKey();
+                        Match match = seasonData.getValue();
+
+                    }
+
+                }
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 
     }
